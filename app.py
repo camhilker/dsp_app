8cee3fdc43f418c6015324eb0366006067fbaa53
Camellia Hilker <camrae324@gmail.com>
2021-07-08 15:13:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,17 +21,19 @@ app.layout = html.Div(children=[
 
     html.Br(),
 
-    html.Div(["Sample 1 Size (n1): ", dcc.Input(id='n1', value=32, type='number')]),
-    html.Div(["Sample 2 Size (n2): ", dcc.Input(id='n2', value=32, type='number')]),
-    html.Div(["Sample 1 Acceptance # (c1): ", dcc.Input(id='c1', value=2, type='number')]),
-    html.Div(["Sample 2 Acceptance # (c2): ", dcc.Input(id='c2', value=6, type='number')]),
-    html.Div(["Reject (r): ", dcc.Input(id='r', value=5, type='number')]),
-    html.Div(["AQL:  ", dcc.Input(id='aql_0', value=0.95, type='number')]),
-    html.Div(["RQL: ", dcc.Input(id='rql_0', value=0.9, type='number')]),
+    html.Div(["Sample 1 Size (n1): ", html.Br(), dcc.Input(id='n1', value=32, type='number'), html.Br(), 
+              "Sample 2 Size (n2): ", html.Br(), dcc.Input(id='n2', value=32, type='number'), html.Br(), 
+              "Sample 1 Acceptance # (c1): ", html.Br(), dcc.Input(id='c1', value=2, type='number'), html.Br(), 
+              "Sample 2 Acceptance # (c2): ", html.Br(), dcc.Input(id='c2', value=6, type='number'), html.Br(), 
+              "Reject (r): ", html.Br(), dcc.Input(id='r', value=5, type='number'), html.Br(), 
+              "AQL:  ", html.Br(), dcc.Input(id='aql_0', value=0.95, type='number'), html.Br(), 
+              "RQL: ", html.Br(), dcc.Input(id='rql_0', value=0.9, type='number')], 
+             
+             style={'float': 'left','margin': 'auto', 'width':'18%', 'padding':'15px'}),
 
-
-
-    dcc.Graph(id='subplots', responsive=False)
+    html.Div(dcc.Graph(id='subplots', responsive=False), 
+             
+             style={'float': 'left','margin': 'auto', 'width':'80%'})
 ])
 
 
@@ -63,9 +65,9 @@ def update_figure(ss_1, ss_2, c_1, c_2, r, aql, rql):
     pass_1 = [i for i in range(c_1+1, r)]
     pass_tup = [(i, j) for i in pass_1 for j in ph if i+j <= c_2]
 
-    #title_string = "Double sampling plan for n=" + str(ss_1) + " and c=" + str(c_1)
+    title_string = "Double sampling plan for n=" + str(ss_1) + " and c=" + str(c_1)
     #plot1_title = "Single Sampling (n=" + str(ss_1) + ", c=" + str(c_1) + ')'
-    plot2_title = "Double Sampling (n1=" + str(ss_1) + ", c1=" + str(c_1) + ') and (n2=' + str(ss_2) + ", c2=" + str(c_2) + ")"
+    plot2_title = "Double Sampling (n=" + str(ss_1) + ", c=" + str(c_1) + ') and (n=' + str(ss_2) + ", c=" + str(c_2) + ")"
 
     fig = make_subplots(subplot_titles=(plot2_title, ' '), shared_yaxes=True, rows=1, cols=2)
     
@@ -88,7 +90,7 @@ def update_figure(ss_1, ss_2, c_1, c_2, r, aql, rql):
     fig.update_xaxes(title_text="Fraction Defective", row=1, col=2)
     fig.update_yaxes(title_text="Probability of Acceptance", row=1, col=1)
 
-    fig.update_layout(height=600, width=1200, showlegend=False, template='plotly_white', hovermode='x unified')
+    fig.update_layout(height=600, width=1500, showlegend=False, template='plotly_white', hovermode='x unified')
 
     return fig
 
