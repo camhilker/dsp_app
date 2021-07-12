c9443be22b133102017e07093dc5950276ce60a4
Camellia Hilker <camrae324@gmail.com>
2021-07-12 11:01:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ app.index_string = """<!DOCTYPE html>
 <html>
     <head>
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1YB8HGWHRM"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-201960300-1"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'G-1YB8HGWHRM');
+            gtag('config', 'UA-201960300-1');
         </script>
         {%metas%}
         <title>{%title%}</title>
